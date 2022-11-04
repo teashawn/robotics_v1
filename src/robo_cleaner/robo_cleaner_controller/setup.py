05de3f3a3e5bfcceca64e82993394ef707ebcb80
Tihomir Petkov <tisho@taxime.to>
2022-11-05 00:02:11 +0200

    Added PYthon dependancies to project configuration

--- src/robo_cleaner/robo_cleaner_controller/setup.py
+++ src/robo_cleaner/robo_cleaner_controller/setup.py
@@ -11,7 +11,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools', 'numpy', 'networkx', 'autologging'],
     zip_safe=True,
     maintainer='tisho',
     maintainer_email='tisho@taxime.to',
